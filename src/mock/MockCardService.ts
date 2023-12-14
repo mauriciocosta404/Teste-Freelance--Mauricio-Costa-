1ec79feb49dd444cf4c1ff48934461cf88459ce2
mauriciocosta404 <mc611641@gmail.com>
2023-12-14 10:09:40 +0100

    feat: adicionando os services cards

---
+++ src/mock/MockCardService.ts
@@ -0,0 +1,17 @@
+import { ServicesType } from "@/types/servicesType"
+export const MockCardService:ServicesType[] = [
+    {
+        title:"PRODUTOS",
+        icon:"",
+        serviceName:"Vidrarias e Equipamentos"
+    },
+    {
+        title:"QUIMICOS",
+        icon:"",
+        serviceName:"Materiais Químicos"
+    },
+    {
+        title:"LABORATÓRIOS",
+        serviceName:"Ferramentas e Utensílios"
+    },
+]  
